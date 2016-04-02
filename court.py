afc2cd22334c29ed4b4af247556e858d4f21fdb8
tankorsmash <tankorsmash@gmail.com>
2016-04-01 22:43:02 -0400

    first commit with instructions

---
+++ court.py
@@ -0,0 +1,15 @@
+__author__ = 'Courtenay'
+
+your_task = """
+create a list of days in the week
+
+for each day of the week print out a different thing you're going to do,
+like on monday you'll walk the dog, on tuesday you'll go to the gym etc
+
+once you've got that going, you'll make it so it asks you what you want to do
+on monday, and then print that along with the preprogrammed stuff
+
+then you'll make it so it asks you the day you want to edit instead of only monday
+
+then you'll make it so it saves it so the next time you start the app, it'll persist what you saved
+"""
